62c1ca5852cf4d49757520b6561191fdfcc2e316
Liuxk <lxk811@gmail.com>
2026-02-11 21:24:37 +0800

    0211

--- control_multi.bat
+++ control_multi.bat
@@ -14,28 +14,28 @@ echo ==========================================
 
 :: 运行 para1
 echo [Task 1] Running with para file...
-%EXE_PATH% D:\VSprojects\TaihuStone\para\rock_scan_rotated_128_90_0.6_3_25_1.7_0.7_0.89_0.35_11011.txt
+%EXE_PATH% D:\VSprojects\TaihuStone\para\boulder_smooth_rotated_128_90_0.6_3_25_1.7_0.7_0.89_0.35_11011.txt
 echo Task 1 Finished.
 echo.
 
 :: 运行 para2 (假设你已经创建了)
 echo [Task 2] Running with para2.txt...
-%EXE_PATH% D:\VSprojects\TaihuStone\para\rock_scan_rotated_128_80_0.6_3_25_1.7_0.7_0.89_0.35_11011.txt
+%EXE_PATH% D:\VSprojects\TaihuStone\para\boulder_smooth_rotated_128_80_0.6_3_25_1.7_0.7_0.89_0.35_11011.txt
 echo Task 2 Finished.
 echo.
 
 echo [Task 3] Running with para3.txt...
-%EXE_PATH% D:\VSprojects\TaihuStone\para\rock_scan_rotated_128_70_0.6_3_25_1.7_0.7_0.89_0.35_11011.txt
+%EXE_PATH% D:\VSprojects\TaihuStone\para\boulder_smooth_rotated_128_70_0.6_3_25_1.7_0.7_0.89_0.35_11011.txt
 echo Task 3 Finished.
 echo.
 
 echo [Task 4] Running with para3.txt...
-%EXE_PATH% D:\VSprojects\TaihuStone\para\rock_scan_rotated_128_60_0.6_3_25_1.7_0.7_0.89_0.35_11011.txt
+%EXE_PATH% D:\VSprojects\TaihuStone\para\boulder_smooth_rotated_128_60_0.6_3_25_1.7_0.7_0.89_0.35_11011.txt
 echo Task 4 Finished.
 echo.
 
 echo [Task 5] Running with para3.txt...
-%EXE_PATH% D:\VSprojects\TaihuStone\para\rock_scan_rotated_128_50_0.6_3_25_1.7_0.7_0.89_0.35_11011.txt
+%EXE_PATH% D:\VSprojects\TaihuStone\para\boulder_smooth_rotated_128_50_0.6_3_25_1.7_0.7_0.89_0.35_11011.txt
 echo Task 5 Finished.
 echo.
 
